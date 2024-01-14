3be116330f98fe608a1bd67912245d9d8dc37a28
Arvind Badwar <arvind.badwar19@gmail.com>
2024-01-14 15:44:36 +0530

    Completed Transaction List Page with Data Grid

--- src/pages/Transactions.jsx
+++ src/pages/Transactions.jsx
@@ -1,60 +1,91 @@
 import { ThemeProvider } from '@emotion/react';
-import { Box, createTheme } from '@mui/material';
+import { Box, Container, createTheme } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
+import { collection, getDocs, query, where } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import { db } from '../firebase';
+import { useSelector } from 'react-redux';
 
 const defaultTheme = createTheme();
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
+  { field: 'date', headerName: 'Date', flex: 1 },
+  { field: 'type', headerName: 'Type', flex: 1 },
+  { field: 'category', headerName: 'Category', flex: 1 },
   {
-    field: 'age',
-    headerName: 'Age',
+    field: 'subCategory',
+    headerName: 'Sub Category',
+    flex: 1,
+  },
+  {
+    field: 'account',
+    headerName: 'Account',
+    flex: 1,
+  },
+  {
+    field: 'amount',
+    headerName: 'Amount',
+    flex: 1,
     type: 'number',
-    width: 90,
   },
   {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
+    field: 'note',
+    headerName: 'Note',
+    flex: 1,
     sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
   },
-];
-
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
 ];
 
 const Transactions = () => {
+  const userId = useSelector((state) => state.user.user.userId);
+  const [transactionList, setTransactionList] = useState([]);
+  const getTransactionList = async () => {
+    const q = query(
+      collection(db, 'transactions'),
+      where('userId', '==', userId)
+    );
+    const querySnapshot = await getDocs(q);
+    let list = [];
+    querySnapshot.forEach((doc) => {
+      const data = doc.data();
+      list.push(data);
+    });
+    setTransactionList(list.sort((a, b) => b.createdAt - a.createdAt));
+  };
+
+  useEffect(() => {
+    getTransactionList();
+  }, []);
+
   return (
     <>
       <ThemeProvider theme={defaultTheme}>
         <Box sx={{ display: 'flex' }}>
-          <div style={{ height: 400, width: '100%' }}>
-            <DataGrid
-              rows={rows}
-              columns={columns}
-              initialState={{
-                pagination: {
-                  paginationModel: { page: 0, pageSize: 5 },
-                },
-              }}
-              pageSizeOptions={[5, 10]}
-              checkboxSelection
-            />
-          </div>
+          <Box
+            component="main"
+            sx={{
+              backgroundColor: (theme) =>
+                theme.palette.mode === 'light'
+                  ? theme.palette.grey[100]
+                  : theme.palette.grey[900],
+              flexGrow: 1,
+              overflow: 'auto',
+            }}
+          >
+            <Container style={{ height: '90vh' }}>
+              <DataGrid
+                rows={transactionList}
+                columns={columns}
+                initialState={{
+                  pagination: {
+                    paginationModel: { page: 0, pageSize: 10 },
+                  },
+                }}
+                pageSizeOptions={[10, 20]}
+                getRowId={(row) => row.transactionId}
+              />
+            </Container>
+          </Box>
         </Box>
       </ThemeProvider>
     </>
